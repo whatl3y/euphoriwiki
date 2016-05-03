9fdb1855d1189717d22bbeb4c2348fd27b1dcb36
Lance Whatley <lwhatley@pmgnet.local>
2016-05-03 19:02:20 -0400

    update 20160503

--- libs/ApiClient.js
+++ libs/ApiClient.js
@@ -4,102 +4,102 @@ var Object = require("../public/js/Object_prototypes.js");
 var config = require("./config.js");
 
 /*-----------------------------------------------------------------------------------------
-|TITLE:		ApiClient.js
+|TITLE:    ApiClient.js
 |PURPOSE:
-|AUTHOR:	Lance Whatley
+|AUTHOR:  Lance Whatley
 |CALLABLE METHODS:
 |ASSUMES:
 |REVISION HISTORY:
-|			*LJW 12/26/2015 - created
+|      *LJW 12/26/2015 - created
 -----------------------------------------------------------------------------------------*/
 function ApiClient(opts) {
-	opts=opts || {};
+  opts=opts || {};
 
-	this.verb = opts.verb || "GET";
-	this.port = opts.port || ((opts.secure) ? 443 : 80);
-	this.clientType = (opts.secure) ? "https" : "http";
-	this.headers = opts.headers || {};
+  this.verb = opts.verb || "GET";
+  this.port = opts.port || ((opts.secure) ? 443 : 80);
+  this.clientType = (opts.secure) ? "https" : "http";
+  this.headers = opts.headers || {};
 
-	this.dummyObject = {};
-	this.dummyObject.http = http;
-	this.dummyObject.https = https;
+  this.dummyObject = {};
+  this.dummyObject.http = http;
+  this.dummyObject.https = https;
 }
 
 /*-----------------------------------------------------------------------------------------
-|NAME:			request (PUBLIC)
-|DESCRIPTION:	Makes an HTTP/HTTPS request based on the settings provided
-|PARAMETERS:	1. obj(OPT): callback function after we get response
-|				2. type(OPT): verb type for an API client
-|SIDE EFFECTS:	Nothing
-|ASSUMES:		Nothing
-|RETURNS:		<string>: If a GET request, a serialized string
-|				false: error getting token
+|NAME:      request (PUBLIC)
+|DESCRIPTION:  Makes an HTTP/HTTPS request based on the settings provided
+|PARAMETERS:  1. obj(OPT): callback function after we get response
+|        2. type(OPT): verb type for an API client
+|SIDE EFFECTS:  Nothing
+|ASSUMES:    Nothing
+|RETURNS:    <string>: If a GET request, a serialized string
+|        false: error getting token
 -----------------------------------------------------------------------------------------*/
 ApiClient.prototype.request = function(reqData,cb) {
-	reqData=reqData || {};
-	reqData=this.params(reqData,this.verb);
+  reqData=reqData || {};
+  reqData=this.params(reqData,this.verb);
 
-	var options={
-		hostname: this.endpoint,
-		path: this.path,
-		protocol: this.clientType+":",
-		port: this.port,
-		method: this.verb,
-		headers: {
-			'Content-Type': 'application/x-www-form-urlencoded'
-		}
-	};
-	if (this.verb==="POST") options.headers["Content-Length"] = reqData.length;
-	options.headers = Object.merge(options.headers,this.headers);
+  var options={
+    hostname: this.endpoint,
+    path: this.path,
+    protocol: this.clientType+":",
+    port: this.port,
+    method: this.verb,
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+  };
+  if (this.verb==="POST") options.headers["Content-Length"] = reqData.length;
+  options.headers = Object.merge(options.headers,this.headers);
 
-	var req = this.dummyObject[this.clientType].request(options,
-		function(res) {
-			var body="";
-			res.on("data",function(chunk) {
-				body+=chunk;
-			});
-			res.on('end',function() {
-				if (typeof cb==="function") cb(null,body);
-			});
-		}
-	).on("error", function(e) {
-		cb(e);
-	});
+  var req = this.dummyObject[this.clientType].request(options,
+    function(res) {
+      var body="";
+      res.on("data",function(chunk) {
+        body+=chunk;
+      });
+      res.on('end',function() {
+        if (typeof cb==="function") cb(null,body);
+      });
+    }
+  ).on("error", function(e) {
+    cb(e);
+  });
 
-	if (this.verb==="POST") req.write(reqData);
-	req.end();
+  if (this.verb==="POST") req.write(reqData);
+  req.end();
 }
 
 /*-----------------------------------------------------------------------------------------
-|NAME:			params (PUBLIC)
-|DESCRIPTION:	Takes
-|PARAMETERS:	1. obj(OPT): callback function after we get response
-|				2. type(OPT): verb type for an API client
-|SIDE EFFECTS:	Nothing
-|ASSUMES:		Nothing
-|RETURNS:		<string>: If a GET request, a serialized string
-|				false: error getting token
+|NAME:      params (PUBLIC)
+|DESCRIPTION:  Takes
+|PARAMETERS:  1. obj(OPT): callback function after we get response
+|        2. type(OPT): verb type for an API client
+|SIDE EFFECTS:  Nothing
+|ASSUMES:    Nothing
+|RETURNS:    <string>: If a GET request, a serialized string
+|        false: error getting token
 -----------------------------------------------------------------------------------------*/
 ApiClient.prototype.params = function(obj,type) {
-	obj=obj || {};
-	type= (typeof type==='string') ? type.toLowerCase() : 'get';
+  obj=obj || {};
+  type= (typeof type==='string') ? type.toLowerCase() : 'get';
 
-	switch(type) {
-		case 'get':
-			if (typeof obj==="string") return obj;
-			return Object.serialize(obj);
+  switch(type) {
+    case 'get':
+      if (typeof obj==="string") return obj;
+      return Object.serialize(obj);
 
-		case 'post':
-			if (typeof obj==="string") return Object.unserialize(obj);
-			return obj;
-	}
+    case 'post':
+      if (typeof obj==="string") return Object.unserialize(obj);
+      return obj;
+  }
 
-	return '';
+  return '';
 }
 
 //-------------------------------------------------------
 //NodeJS
 if (typeof module !== 'undefined' && module.exports) {
-	module.exports=ApiClient;
+  module.exports=ApiClient;
 }
 //-------------------------------------------------------
--- libs/GeoIP.js
+++ libs/GeoIP.js
@@ -1,56 +1,56 @@
 var httpClient = require("./ApiClient.js");
 
 /*-----------------------------------------------------------------------------------------
-|TITLE:		GeoIP.js
-|PURPOSE:	Uses freegeoip.net API to take an IP address or hostname and finds it's physical
-|			location. Find out more information at freegeoip.net.
-|AUTHOR:	Lance Whatley
+|TITLE:    GeoIP.js
+|PURPOSE:  Uses freegeoip.net API to take an IP address or hostname and finds it's physical
+|      location. Find out more information at freegeoip.net.
+|AUTHOR:  Lance Whatley
 |CALLABLE METHODS:
-|			go: makes a request to the endpoint
-|REVISION HISTORY:	
-|			*LJW 1/7/2016 - created
+|      go: makes a request to the endpoint
+|REVISION HISTORY:  
+|      *LJW 1/7/2016 - created
 -----------------------------------------------------------------------------------------*/
 function GeoIP(dataType) {
-	this.dataType = dataType || "json";		//json, csv, xml, jsonp
-	this.client = new httpClient();
-	this.client.endpoint = "freegeoip.net";
+  this.dataType = dataType || "json";    //json, csv, xml, jsonp
+  this.client = new httpClient();
+  this.client.endpoint = "freegeoip.net";
 }
 
 /*-----------------------------------------------------------------------------------------
-|NAME:			setPath (PUBLIC)
-|DESCRIPTION:	Sets the path of the geofinder to make the http request
-|PARAMETERS:	1. ip(OPT): IP address
-|				2. cb(OPT): callback
-|SIDE EFFECTS:	Nothing
-|ASSUMES:		Nothing
-|RETURNS:		<string>: the new path
+|NAME:      setPath (PUBLIC)
+|DESCRIPTION:  Sets the path of the geofinder to make the http request
+|PARAMETERS:  1. ip(OPT): IP address
+|        2. cb(OPT): callback
+|SIDE EFFECTS:  Nothing
+|ASSUMES:    Nothing
+|RETURNS:    <string>: the new path
 -----------------------------------------------------------------------------------------*/
 GeoIP.prototype.setPath = function(ip) {
-	return this.client.path = "/" + this.dataType + "/" + ip;
+  return this.client.path = "/" + this.dataType + "/" + ip;
 }
 
 /*-----------------------------------------------------------------------------------------
-|NAME:			go (PUBLIC)
-|DESCRIPTION:	Executes an http request to the endpoint
-|PARAMETERS:	1. ip(OPT): IP address
-|				2. cb(OPT): callback function to take data and do something with it
-|SIDE EFFECTS:	Nothing
-|ASSUMES:		Nothing
-|RETURNS:		Nothing
+|NAME:      go (PUBLIC)
+|DESCRIPTION:  Executes an http request to the endpoint
+|PARAMETERS:  1. ip(OPT): IP address
+|        2. cb(OPT): callback function to take data and do something with it
+|SIDE EFFECTS:  Nothing
+|ASSUMES:    Nothing
+|RETURNS:    Nothing
 -----------------------------------------------------------------------------------------*/
 GeoIP.prototype.go = function(ip,cb) {
-	var self = this;
-	
-	this.setPath(ip);
-	this.client.request(null,function(err,data) {
-		if (err) cb(err);
-		else cb(null,(self.dataType=="json") ? JSON.parse(data) : data);
-	});
+  var self = this;
+  
+  this.setPath(ip);
+  this.client.request(null,function(err,data) {
+    if (err) cb(err);
+    else cb(null,(self.dataType=="json") ? JSON.parse(data) : data);
+  });
 }
 
 //-------------------------------------------------------
 //NodeJS
 if (typeof module !== 'undefined' && module.exports) {
-	module.exports=GeoIP;
+  module.exports=GeoIP;
 }
 //-------------------------------------------------------
