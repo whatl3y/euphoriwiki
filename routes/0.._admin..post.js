775aaad686b1955d93ac1b5be0eb26cdc4079b0d
Lance Whatley <lwhatley@pmgnet.local>
2016-05-24 18:29:13 -0400

    update 20160524

--- routes/0.._admin..post.js
+++ routes/0.._admin..post.js
@@ -30,47 +30,79 @@
       var value = info.value;
       value = Object.removeDollarKeys(value);
       
-      async.series([
-        function(callback) {
-          config.mongodb.db.collection("adminsettings").update({domid:settingKey},{$set:{value:value}},function(err) {
-            if (err) res.json({success:false, error:"There was an issue saving your setting. Please check the logs or try again."});
-            else res.json({success:true});
+      var A = new Authentication();
+      
+      var saveSetting = function(value) {
+        async.series([
+          function(callback) {
+            config.mongodb.db.collection("adminsettings").update({domid:settingKey},{$set:{value:value}},function(err) {
+              if (err) res.json({success:false, error:"There was an issue saving your setting. Please check the logs or try again."});
+              else res.json({success:true});
+              
+              callback(err);
+            });
+          },
+          function(callback) {
+            config.mongodb.db.collection("adminsettings").find().toArray(function(e,settings) {
+              callback(e,settings);
+            });
+          },
+          function(callback) {
+            new WikiHandler().initQueries(function(err,data) {
+              callback(err,data);
+            });
+          }
+        ],
+          function(err,results) {
+            if (err) return log.error(err);
             
-            callback(err);
-          });
-        },
-        function(callback) {
-          config.mongodb.db.collection("adminsettings").find().toArray(function(e,settings) {
-            callback(e,settings);
-          });
-        },
-        function(callback) {
-          new WikiHandler().initQueries(function(err,data) {
-            callback(err,data);
-          });
-        }
-      ],
-        function(err,results) {
-          if (err) return log.error(err);
-          
-          var settings = results[1];
-          var queries = results[2].queries;
-          var oData = results[2].oData;
-          
-          //reset config cache and execute initial queries
-          //that will reset information in config file
-          config.admin.SETTINGS = settings;
-          _.each(queries,function(queryInfo) {
-            if (queryInfo.extracode) {
-              try {
-                eval(queryInfo.extracode);
-              } catch(err) {
-                log.error(err);
+            var settings = results[1];
+            var queries = results[2].queries;
+            var oData = results[2].oData;
+            
+            //reset config cache and execute initial queries
+            //that will reset information in config file
+            config.admin.SETTINGS = settings;
+            _.each(queries,function(queryInfo) {
+              if (queryInfo.extracode) {
+                try {
+                  eval(queryInfo.extracode);
+                } catch(err) {
+                  log.error(err);
+                }
               }
-            }
+            });
+          }
+        );
+      }
+      
+      
+      if (value instanceof Array && typeof value[0] === "object" && value[0].username) {
+        var newValue = [];
+        
+        async.each(value,function(v,callback) {
+          A.findOrSaveUser({username:v.username},function(e,userInfo) {
+            if (e) return callback(e);
+            
+            if (userInfo) {
+              newValue.push({
+                username: userInfo.username,
+                firstname: userInfo.firstname,
+                lastname: userInfo.lastname
+              })
+            } else newValue.push({username:v.username});
+            
+            return callback();
           });
-        }
-      );
+        },
+          function(err) {
+            if (err) return log.error(err);
+            
+            saveSetting(newValue);
+          }
+        );
+      } else saveSetting(value);
+      
       
       break;
     
