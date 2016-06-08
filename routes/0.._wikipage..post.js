83d54f934490b2bf95c5a5acbd6e8c8084d9a66f
Lance Whatley <lwhatley@pmgnet.local>
2016-06-08 09:51:58 -0400

    update 20160608

--- routes/0.._wikipage..post.js
+++ routes/0.._wikipage..post.js
@@ -929,66 +929,65 @@
       break;
     
     case "updatePath":
-      if (!A.isLoggedIn()) res.json({success:false, error:"You must be logged in to perform this function. Please log in and try again."});
-      else {
-        var newPath = info.newPath;
-        newPath = (newPath.indexOf("/") == 0) ? newPath : "/"+newPath;
-        
-        async.parallel([
-          function(callback) {
-            wiki.allowedPath(newPath,function(e,isAllowed) {
-              callback(e,isAllowed);
-            });
-          },
-          function(callback) {
-            wiki.getPage({filters:{path:newPath},fields:{path:1}},function(e,page) {
-              callback(e,page);
-            });
-          },
-          function(callback) {
-            Access.isAdmin({username:username, path:wiki.path},function(e,isAdmin) {
-              callback(e,isAdmin);
-            });
+      if (!A.isLoggedIn()) return res.json({success:false, error:"You must be logged in to perform this function. Please log in and try again."});
+      
+      var newPath = info.newPath;
+      newPath = (newPath.indexOf("/") == 0) ? newPath : "/"+newPath;
+      
+      async.parallel([
+        function(callback) {
+          wiki.allowedPath(newPath,function(e,isAllowed) {
+            callback(e,isAllowed);
+          });
+        },
+        function(callback) {
+          wiki.getPage({filters:{path:newPath},fields:{path:1}},function(e,page) {
+            callback(e,page);
+          });
+        },
+        function(callback) {
+          Access.isAdmin({username:username, path:wiki.path},function(e,isAdmin) {
+            callback(e,isAdmin);
+          });
+        }
+      ],
+        function(err,results) {
+          if (err) {
+            res.json({success:false, error:"There was an issue trying to update your path. Please try again."});
+            return log.error(err);
           }
-        ],
-          function(err,results) {
-            if (err) {
-              res.json({success:false, error:"There was an issue trying to update your path. Please try again."});
-              return log.error(err);
+          
+          var allowed = results[0];
+          var page = results[1];
+          var canUpdate = results[2];
+          
+          if (!canUpdate) return res.json({success:false, error:"You cannot update the path as you do not have appropriate rights. Contact the page administrator to get access."});
+          else if (!allowed) return res.json({success:false, error:"Path " + newPath + " is not a valid path to change to. Please try another path."});
+          else if (page.length) return res.json({success:false, error:"The path, " + newPath + ", already exists and can't be overwritten. Please try another path or delete/move the page at " + newPath + " and try again."});
+          
+          async.parallel([
+            function(callback) {
+              config.mongodb.db.collection("wikicontent").update({path:wiki.path},{$set:{path:newPath, updated:new Date()}},function(err) {
+                callback(err);
+              });
+            },
+            function(callback) {
+              config.mongodb.db.collection("wiki_modules_instances").update({path:wiki.path},{$set:{path:newPath}},function(err) {
+                callback(err);
+              });
             }
-            
-            var allowed = results[0];
-            var page = results[1];
-            var canUpdate = results[2];
-            
-            if (!canUpdate) return res.json({success:false, error:"You cannot update the path as you do not have appropriate rights. Contact the page administrator to get access."});
-            else if (!allowed) return res.json({success:false, error:"Path " + newPath + " is not a valid path to change to. Please try another path."});
-            else if (page.length) return res.json({success:false, error:"The path, " + newPath + ", already exists and can't be overwritten. Please try another path or delete/move the page at " + newPath + " and try again."});
-            
-            async.parallel([
-              function(callback) {
-                config.mongodb.db.collection("wikicontent").update({path:wiki.path},{$set:{path:newPath}},function(err) {
-                  callback(err);
-                });
-              },
-              function(callback) {
-                config.mongodb.db.collection("wiki_modules_instances").update({path:wiki.path},{$set:{path:newPath}},function(err) {
-                  callback(err);
-                });
-              }
-            ],
-              function(err,results) {
-                if (err) return res.json({success:false, error:err});
-                
-                res.json({success:true});
-                
-                audit.log({type:"Update Page Path", additional:{formerPath:wiki.path, newPath:newPath}});
-                wiki.event({type:"updatepagepath", params:{username:username, formerPath:wiki.path, newPath:newPath}},function(e,result) {if (e) log.error(e);});
-              }
-            );
-          }
-        );
-      }
+          ],
+            function(err,results) {
+              if (err) return res.json({success:false, error:err});
+              
+              res.json({success:true});
+              
+              audit.log({type:"Update Page Path", additional:{formerPath:wiki.path, newPath:newPath}});
+              wiki.event({type:"updatepagepath", params:{username:username, formerPath:wiki.path, newPath:newPath}},function(e,result) {if (e) log.error(e);});
+            }
+          );
+        }
+      );
       
       break;
     
