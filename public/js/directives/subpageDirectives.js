e1a62e90e5f343ec59afabfa6239833bfea75dcf
Lance Whatley <whatl3y@gmail.com>
2016-06-23 13:27:27 +0000

    formatting

--- public/js/directives/subpageDirectives.js
+++ public/js/directives/subpageDirectives.js
@@ -8,7 +8,7 @@ function subpageDir() {
       link: "=",
       showChildren: "="
     },
-    template: "<ul class='list-unstyled list-left subpages'><subpage-member data-ng-repeat='(key,page) in subpages' subpage-member-key='key' subpage-member-val='page' subpage-member-link='link || \"\"' show-children='showChildren'></subpage-member></ul>"
+    template: "<ul class='list-unstyled list-left-lg subpages'><subpage-member data-ng-repeat='(key,page) in subpages' subpage-member-key='key' subpage-member-val='page' subpage-member-link='link || \"\"' show-children='showChildren'></subpage-member></ul>"
   }
 }
 
@@ -27,7 +27,7 @@ function subpageMemberDir($compile) {
     link: function($scope, $element, $attrs) {
       $scope.showDirectChildren = $scope.showChildren;
       $scope.showSelf = (!$scope.subpageMemberLink) ? true : !!$scope.$parent.showChildren;
-      
+
       if (angular.isObject($scope.subpageMemberVal) && Object.size($scope.subpageMemberVal)) {
         $element.append("<subpage subpages='subpageMemberVal' link='subpageMemberLink + " + '"/"' + " + subpageMemberKey' show-children='showChildren'></subpage>");
         $compile($element.contents())($scope);
@@ -39,21 +39,21 @@ function subpageMemberDir($compile) {
       $scope.$on("showDirectChildrenEvent",function(e,data) {
         var link = data.link;
         var show = data.showOrHide;
-        
+
         if (link.split("/").length == $scope.subpageMemberLink.split("/").length-1) {
           if (show) $scope.showSelf = true;
           else $scope.showSelf = false;
         }
       });
-      
+
       $scope.functions = {
         showOrHideChildren: function() {
           if (!$scope.expanded) {
             $scope.expanded = true;
             $scope.showDirectChildren = !$scope.showDirectChildren;
-            
+
             $scope.$broadcast("showDirectChildrenEvent",{link:$scope.subpageMemberLink, showOrHide:$scope.showDirectChildren});
-            
+
             setTimeout(function() {
               $scope.$apply(function() {
                 $scope.expanded = false;
