4950ad1617dec22096d37ba047bf342dfddb006b
Lance Whatley <whatl3y@gmail.com>
2016-08-02 09:52:16 -0400

    support additional oauth_scopes

--- routes/0.._login_facebook..get.js
+++ routes/0.._login_facebook..get.js
@@ -1,3 +1,9 @@
 var passport = require("passport");
+var config = require("../config.js");
 
-module.exports = passport.authenticate("facebook",{scope: ['email']});
+var additionalScopes = [];
+if (config.oauth_scopes instanceof Array) {
+  additionalScopes = config.oauth_scopes.filter(function(s) { return s.type == 'facebook' }).map(function(s) { return s.scope });
+}
+
+module.exports = passport.authenticate("facebook",{scope: [].concat(['email'],additionalScopes)});
--- routes/0.._login_google..get.js
+++ routes/0.._login_google..get.js
@@ -1,3 +1,9 @@
 var passport = require("passport");
+var config = require("../config.js");
 
-module.exports = passport.authenticate("google",{scope: ["profile", "email"]});
+var additionalScopes = [];
+if (config.oauth_scopes instanceof Array) {
+  additionalScopes = config.oauth_scopes.filter(function(s) { return s.type == 'google' }).map(function(s) { return s.scope });
+}
+
+module.exports = passport.authenticate("google",{scope: [].concat(["profile", "email"],additionalScopes)});
