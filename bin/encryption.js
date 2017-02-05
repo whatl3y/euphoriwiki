fadee55497fa345a784afc5e174cec210b65a818
Lance Whatley <whatl3y@gmail.com>
2017-02-05 11:28:17 -0500

    support output filepath for encryption

--- bin/encryption.js
+++ bin/encryption.js
@@ -1,19 +1,31 @@
-var Encryption = require("../libs/Encryption.js");
+var argv = require('minimist')(process.argv.slice(2))
+var fs = require('fs')
+var Encryption = require('../libs/Encryption.js')
 
 //filepath should be in 3rd param
 //    EXAMPLE CALL: >node encryption [-d][-e] string
-var which = process.argv[2];
-var text = process.argv[3];
+const encryptText = argv.e || argv.encrypt
+const decryptText = argv.d || argv.decrypt
+const outputFilePath = argv.o || argv.output
+const which = (encryptText) ? 'e' : 'd'
+var text = encryptText || decryptText
 
 switch (which) {
-  case "-e":
-    console.log(new Encryption().encrypt(text));
+  case 'e':
+    write(new Encryption().encrypt(text), outputFilePath)
     break;
 
-  case "-d":
-    console.log(new Encryption().decrypt(text));
+  case 'd':
+    write(new Encryption().decrypt(text), outputFilePath)
     break;
 
   default:
-    console.log("Please enter an argument, -e to encrypt, -d to decrypt.")
+    console.log('Please enter an argument, -e to encrypt, -d to decrypt.')
+}
+
+function write(text, filePath=null) {
+  if (filePath) {
+    return fs.writeFileSync(filePath, text)
+  }
+  console.log(text)
 }
